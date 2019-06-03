2eb6feaf9ca3614e9a88291b1c459c1a517ada4d
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 17:50:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,12 +32,23 @@ def count_contestants_by_hometown(data, hometown)
   counter
 end
 
+
 def get_occupation(data, hometown)
-  
+  data.each do |season_num, contestant|
+    contestant.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash['occupation']
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  num_of_contestants = 0
+    data[season].each do |contestant_hash|
+      total_age += (contestant_hash["age"]).to_f
+      num_of_contestants += 1
+    end
+    (total_age/num_of_contestants).round
 end
-
-
